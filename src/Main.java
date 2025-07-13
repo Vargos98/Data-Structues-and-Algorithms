bd8874c1432e832f2eeede6791110941910236f7
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2025-07-13 11:21:38 +0530

    Add input and print functionality to Main.java
    Replaces the static 'Hello, World!' output with code that prompts the user for the number of integers, reads them into an array, and prints them. Introduces use of Scanner for user input.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int[] arr;
+        arr = new int[100];
+        System.out.print("Please enter the number of numbers you want to print : ");
+        int n = sc.nextInt();
+
+        for(int i=0;i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i =0;i<n;i++)
+        {
+            System.out.print(arr[i] +" ");
+        }
+
     }
 }
